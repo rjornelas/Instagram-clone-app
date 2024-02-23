62bc55e38c62d015dd2993ca5706c21f1a7af42c
rjornelas <rafael.j.ornelas@gmail.com>
2024-02-23 11:10:19 -0300

    Created dialog to choose option to select profile photo (Gallery or camera)

--- app/src/main/java/rjornelas/course/instagram/register/view/RegisterPhotoFragment.kt
+++ app/src/main/java/rjornelas/course/instagram/register/view/RegisterPhotoFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import rjornelas.course.instagram.R
+import rjornelas.course.instagram.common.view.CustomerDialog
 
 class RegisterPhotoFragment : Fragment() {
 
@@ -15,5 +16,24 @@ class RegisterPhotoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_register_photo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val customerDialog = CustomerDialog(requireContext())
+
+        customerDialog.setTitle(R.string.photo_register_title)
+        customerDialog.addButton(R.string.photo, R.string.gallery){
+            when(it.id){
+                R.string.photo -> {
+                    //open camera
+                }
+                R.string.gallery -> {
+                    //open gallery
+                }
+            }
+        }
+        customerDialog.show()
     }
 }

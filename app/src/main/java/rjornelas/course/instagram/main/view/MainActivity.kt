0ca3cdee6400a858b618e4a50578f27039398df3
rjornelas <rafael.j.ornelas@gmail.com>
2024-02-25 15:14:37 -0300

    Custom Activity_Main status bar color

--- app/src/main/java/rjornelas/course/instagram/main/view/MainActivity.kt
+++ app/src/main/java/rjornelas/course/instagram/main/view/MainActivity.kt
@@ -1,12 +1,23 @@
 package rjornelas.course.instagram.main.view
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsetsController
+import androidx.core.content.ContextCompat
 import rjornelas.course.instagram.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        }
+        window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
     }
 }

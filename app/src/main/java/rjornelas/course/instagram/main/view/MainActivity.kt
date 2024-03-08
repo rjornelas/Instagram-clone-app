3f25c6dee0bbd1d292f355f190a3ff26494c2bd7
rjornelas <rafael.j.ornelas@gmail.com>
2024-03-08 20:10:46 -0300

    Fragment Stacking

--- app/src/main/java/rjornelas/course/instagram/main/view/MainActivity.kt
+++ app/src/main/java/rjornelas/course/instagram/main/view/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     private lateinit var searchFragment: Fragment
     private lateinit var cameraFragment: Fragment
     private lateinit var profileFragment: Fragment
-    private var currentFragment: Fragment? = null
+    private lateinit var currentFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,8 +54,19 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         cameraFragment = CameraFragment()
         profileFragment = ProfileFragment()
 
+        currentFragment = homeFragment
+
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.main_fragment, profileFragment, "3").hide(profileFragment)
+            add(R.id.main_fragment, cameraFragment, "2").hide(cameraFragment)
+            add(R.id.main_fragment, searchFragment, "1").hide(searchFragment)
+            add(R.id.main_fragment, homeFragment, "0").hide(homeFragment)
+            commit()
+        }
+
+
         binding.mainBottomNav.setOnNavigationItemSelectedListener(this)
-        binding.mainBottomNav.selectedItemId = R.id.menu_bottom_home
+//        binding.mainBottomNav.selectedItemId = R.id.menu_bottom_home
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -65,22 +76,26 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         when (item.itemId) {
             R.id.menu_bottom_home -> {
                 if(currentFragment == homeFragment) return false
+                supportFragmentManager.beginTransaction().hide(currentFragment).show(homeFragment).commit()
                 currentFragment = homeFragment
 
             }
 
             R.id.menu_bottom_search -> {
                 if(currentFragment == searchFragment) return false
+                supportFragmentManager.beginTransaction().hide(currentFragment).show(searchFragment).commit()
                 currentFragment = searchFragment
             }
 
             R.id.menu_bottom_add -> {
                 if(currentFragment == cameraFragment) return false
+                supportFragmentManager.beginTransaction().hide(currentFragment).show(cameraFragment).commit()
                 currentFragment = cameraFragment
             }
 
             R.id.menu_bottom_profile -> {
                 if(currentFragment == profileFragment) return false
+                supportFragmentManager.beginTransaction().hide(currentFragment).show(profileFragment).commit()
                 currentFragment = profileFragment
                 scrollToolbarEnabled = true
             }
@@ -88,9 +103,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
         setScrollToolbarEnabled(scrollToolbarEnabled)
 
-        currentFragment?.let {
-            replaceFragment(R.id.main_fragment, it)
-        }
+//        currentFragment?.let {
+//            replaceFragment(R.id.main_fragment, it)
+//        }
         return true
     }
 
